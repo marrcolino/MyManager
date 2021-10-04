49c00bf46e0a84b930d4fb4b29cfd760eadc6e2e
Marrcolino <marcoooo_97@hotmail.it>
2021-10-04 12:45:37 +0100

    modifica

--- app/src/main/java/com/example/mymanager/MainActivity.java
+++ app/src/main/java/com/example/mymanager/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.mymanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
